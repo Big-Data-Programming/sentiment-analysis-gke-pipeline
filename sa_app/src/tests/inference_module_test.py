d771bd405886573b916312456b1c67d9487c52d5
unknown <prabhupad26@gmail.com>
2024-09-29 13:40:19 +0200

    improvements in inference model

--- sa_app/src/tests/inference_module_test.py
+++ sa_app/src/tests/inference_module_test.py
@@ -2,20 +2,16 @@ import os
 
 import pytest
 import torch
-import wandb
 import yaml
 from sa_app.inference.inference import InferenceEngine
+
+import wandb
 
 
 @pytest.fixture
 def sample_config():
-    config = yaml.safe_load(open("tests/test_data/test_app_cfg.yml", "r"))
-    return config
-
-
-def test_inference(sample_config):
     wandb.login(key=os.getenv("WANDB_API_KEY"))
-    config = sample_config
+    config = yaml.safe_load(open("tests/test_data/test_app_cfg.yml", "r"))
     device_in_use = "cuda" if torch.cuda.is_available() else "cpu"
     # Example usage
     ie_obj = InferenceEngine(
@@ -24,7 +20,21 @@ def test_inference(sample_config):
         dataset_params=config["dataset_params"],
         device=device_in_use,
     )
+    return ie_obj
+
+
+def test_inference_negative(sample_config):
+    engine_obj = sample_config
     input_sentence = "I feel so bad today . Such a bad day :( "
-    predicted_labels = ie_obj.perform_inference(input_sentence)
+    predicted_labels = engine_obj.perform_inference(input_sentence)
     assert isinstance(predicted_labels, str) is True
-    assert predicted_labels == "negative"
+
+
+def test_inference_positive(sample_config):
+    engine_obj = sample_config
+    input_sentence = """
+    @swikey haha, okay, i feel much better now.
+    let's just dye our hair paramore red!
+    """
+    predicted_labels = engine_obj.perform_inference(input_sentence)
+    assert isinstance(predicted_labels, str) is True
